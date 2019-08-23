2b48ff93d74a7ecf243d4e60ddad01fa3ef7cc5b
Schoschana <liya.schoschana@gmail.com>
2019-08-24 04:47:44 +0700

    first commit

---
+++ Lotos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lotos
+//
+//  Created by Lili on 24/08/2019.
+//  Copyright © 2019 Lily. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
